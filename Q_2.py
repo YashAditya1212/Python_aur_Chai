c529a4e83d837fdaa5de685fa6b9d2e48be8a664
YashAditya1212 <uchihamadara12134@gmail.com>
2026-03-09 01:03:56 +0530

    Python Problems

---
+++ Q_2.py
@@ -0,0 +1,9 @@
+import time 
+wait_time=1
+max_retires=5
+attempts=0
+
+while (attempts<max_retires):
+    print("Attempt", attempts+1, "wait_time", wait_time, )
+    time.sleep(wait_time)
+    wait_time*=2 
